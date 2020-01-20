5fe9d0613264329327b198dd69ebe79d9b39ee87
elad <elad@copyleaks.com>
2020-01-20 10:48:09 +0200

    fix embedding bug

--- main.py
+++ main.py
@@ -142,10 +142,12 @@ def main():
         embedding = GloveEmbedding("./models/glove/glove.6B.50d.txt", 50)
         layers = get_layers_resid_small()
         batch_size = 5
+        print("Running train with low memory preset")
     else:
         embedding = GloveEmbedding("./models/glove/glove.6B.300d.txt", 300)
         layers = get_layers_resid()
         batch_size = 400
+        print("Running train with high memory preset")
 
     train_and_eval(embedding, layers, batch_size)
 
