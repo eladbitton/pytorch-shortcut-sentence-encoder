acbf6a2ae5300aa9360858f2acf7cd61b8b05956
elad <elad@copyleaks.com>
2020-01-21 18:11:06 +0200

    better main

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_device():
         return torch.device("cpu")
 
 
-def get_layers_small():
+def get_layers_shortcut_small():
     layers = [LSTMLayer(hidden_size=16,
                         num_layers=1,
                         bidirectional=True),
@@ -33,19 +33,20 @@ def get_layers_small():
     return layers
 
 
-def get_layers():
+def get_layers_shortcut():
     layers = [LSTMLayer(hidden_size=512,
                         num_layers=1,
                         bidirectional=True),
-              LSTMLayer(hidden_size=768,
+              LSTMLayer(hidden_size=1024,
                         num_layers=1,
                         bidirectional=True),
-              LSTMLayer(hidden_size=1024,
+              LSTMLayer(hidden_size=2048,
                         num_layers=1,
                         bidirectional=True)
               ]
 
     return layers
+
 
 def get_layers_resid_small():
     layers = [LSTMLayer(hidden_size=30,
@@ -77,7 +78,7 @@ def get_layers_resid():
     return layers
 
 
-def train_and_eval(embedding, layers, batch_size):
+def train_and_eval(embedding, layers, batch_size, layers_type):
     # Device
     device = get_device()
 
@@ -100,7 +101,7 @@ def train_and_eval(embedding, layers, batch_size):
                                 max_sentence_length=Data.MAX_SENTENCE_SIZE,
                                 hidden_mlp=800,
                                 device=device,
-                                layers_type=LayersType.Residual)
+                                layers_type=layers_type)
     model = model.to(device)
 
     # Create optimizer
@@ -114,11 +115,11 @@ def train_and_eval(embedding, layers, batch_size):
 
     # Train the model
     model_trainer.train(train_data, dev_data,
-                        train_log_file='train_resid_1.txt', dev_log_file='dev_resid_1.txt',
+                        train_log_file='train_1.txt', dev_log_file='dev_1.txt',
                         epochs=epochs, batch_size=batch_size)
 
     # Save the model
-    model_trainer.save_model('./models/model_resid_1')
+    model_trainer.save_model('./models/model_1')
 
     # Test the model
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
@@ -128,28 +129,52 @@ def train_and_eval(embedding, layers, batch_size):
                                      output_size=model.output_size)
     model_trainer.eval(test_loader, test_performencer)
     test_performencer.pinpoint()
-    test_performencer.log_to_file('test_resid_1.txt')
+    test_performencer.log_to_file('test_1.txt')
 
 
 def main():
     import sys
-    if len(sys.argv) == 2:
-        low_mem = sys.argv[1] == '--low_mem'
-    else:
-        low_mem = False
+
+    if len(sys.argv) != 3:
+        print("Please supply the necessary args. Run: python ./main.py [--low_mem|--high_mem] [--residual|--shortcut]")
+        return
+
+    if sys.argv[1] != '--low_mem' and sys.argv[1] != '--high_mem':
+        print(
+            "Invalid argument: %s. Run: python ./main.py [--low_mem|--high_mem] [--residual|--shortcut]" % sys.argv[1])
+        return
+
+    if sys.argv[2] != '--residual' and sys.argv[2] != '--shortcut':
+        print(
+            "Invalid argument: %s. Run: python ./main.py [--low_mem|--high_mem] [--residual|--shortcut]" % sys.argv[2])
+        return
+
+    low_mem = sys.argv[1] == '--low_mem'
+    residual = sys.argv[2] == '--residual'
 
     if low_mem:
         print("Running train with low memory preset")
         embedding = GloveEmbedding("./models/glove/glove.6B.50d.txt", 50)
-        layers = get_layers_resid_small()
         batch_size = 5
     else:
         print("Running train with high memory preset")
         embedding = GloveEmbedding("./models/glove/glove.6B.300d.txt", 300)
-        layers = get_layers_resid()
-        batch_size = 500
+        batch_size = 200
 
-    train_and_eval(embedding, layers, batch_size)
+    if residual:
+        layers_type = LayersType.Residual
+        if low_mem:
+            layers = get_layers_resid_small()
+        else:
+            layers = get_layers_resid()
+    else:
+        layers_type = LayersType.Shortcut
+        if low_mem:
+            layers = get_layers_shortcut_small()
+        else:
+            layers = get_layers_shortcut()
+
+    train_and_eval(embedding, layers, batch_size, layers_type)
 
 
 if __name__ == "__main__":
